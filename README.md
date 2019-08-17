81dcf18981950b0935045da8e9191110e9d7bd75
Prasamsha Sharma <prasamshari@gmail.com>
2019-08-16 18:10:19 -0700

    Crystal Collector Game

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # unit-4-game
+Crystal Collector Game!
+Overview:
+This is a fun game where :
+You will be given a random number at the start of the game.
+There are four crystals below.
+By clicking on a crystal you will add a specific amount of points to your total score.
+You win the game by matching your total score to random number,you lose the game if your total score goes above the random number.
+The value of each crystal is hidden from you untill you click on it.
+Each time when the game starts, the game will change the values of each crystal.
+
+Instruction of the game:
+1. The player will have to guess the numbers. 
+
+2. Here's how the app works:
+
+   * There will be four crystals displayed as buttons on the page.
+
+   * The player will be shown a random number at the start of the game.
+
+   * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+
+     * Your game will hide this amount until the player clicks a crystal.
+     * When they do click one, update the player's score counter.
+
+   * The player wins if their total score matches the random number from the beginning of the game.
+
+   * The player loses if their score goes above the random number.
+
+   * The game restarts whenever the player wins or loses.
+
+     * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+
+   * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
+   
+Used HTML, CSS, Javascript, JQuery.
+
+Enjoy playing in the link below:
+
+https://sharmap1.github.io/unit-4-game/.
---
+++ assets/javascript/game.js
@@ -0,0 +1,79 @@
+
+        // There will be four crystals displayed as buttons on the page.
+        var pink = Math.floor(Math.random() * 12) + 1; 
+        var yellow = Math.floor(Math.random() * 12) + 1; 
+        var purple = Math.floor(Math.random() * 12) + 1; 
+        var green = Math.floor(Math.random() * 12) + 1;
+        //    * The player will be shown a random number at the start of the game.
+        // var randomNumber = Math.floor(Math.random() * 12) + 1; // crystal random number
+        var masterNumber= Math.floor((Math.random() * 120) + 19);
+        var addition = 0;
+        var wins = 0;
+        var losses = 0;
+        //    * When they do click one, update the player's score counter.
+         var updateAddition = function() {
+            $("#wins").html("Wins: "+ wins);
+            $("#losses").html("Losses: "+ losses);
+            $(".addition").html(addition);
+        }     
+        //    * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+        //    * Your game will hide this amount until the player clicks a crystal.
+        //    * The player wins if their total score matches the random number from the beginning of the game.
+        //    * The player loses if their score goes above the random number.
+        //    * The game restarts whenever the player wins or loses.
+        //    * logic:
+        var logic = function() {
+        if (addition === masterNumber) {
+            wins++;
+            restart();
+        }else if(addition > masterNumber) {
+            losses++;
+            restart();
+        }else{
+            updateAddition();
+        };
+      };
+        // restart
+        var restart = function () {
+            pink = Math.floor((Math.random() * 12) + 1); 
+            yellow = Math.floor((Math.random() * 12) + 1); 
+            purple = Math.floor((Math.random() * 12) + 1); 
+            green = Math.floor((Math.random() * 12) + 1);
+            // $(".crystal").each(function(){ // built in for loop for jquery
+            //   var randomNumber = Math.floor((Math.random() * 12) + 1);
+            //   $(this).attr("crystalValue", randomNumber)
+            // })
+            masterNumber= Math.floor((Math.random() * 120) + 19);
+            $("#masterNumber").empty();
+            $("#masterNumber").append(masterNumber);
+            addition = 0;
+            updateAddition();
+        };     
+        $(document).ready(function() {
+            restart();
+            // $(".crystal").on("click", function() {
+            //   var crystalValue =  $(this).attr("crystalValue")
+            //   console.log(crystalValue)
+            //   addition  = addition + parseInt(crystalValue);
+            //   console.log(addition)
+            //   logic();  
+            // });
+            $("#pink").on("click", function() {
+              console.log(addition)
+            addition  = addition + pink;  
+            logic();
+            });
+            $("#yellow").on("click", function() {
+              addition = addition + yellow;
+              logic();
+            });
+            $("#purple").on("click", function() {
+              addition  = addition + purple;
+              logic();
+            });
+            $("#green").on("click", function() {
+              addition = addition + green;
+              logic();
+            });
+        }); 
+    
